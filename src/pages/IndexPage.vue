37a1475a0b0e91927936c564c1ce0a1c7bb86dc6
Keaifa <keaifafafa@qq.com>
2023-07-18 20:28:31 +0800

    添加门面模式

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="index-page">
     <a-input-search
-      v-model:value="searchParams.text"
+      v-model:value="searchText"
       placeholder="请输入搜索关键词"
       enter-button="Search"
       size="large"
@@ -38,6 +38,7 @@ import PictureList from "@/components/PictureList.vue";
 import MyDivider from "@/components/MyDivider.vue";
 import { useRoute, useRouter } from "vue-router";
 import myAxios from "@/plugins/myAxios";
+import { message } from "ant-design-vue";
 
 const postList = ref([]);
 const userList = ref([]);
@@ -49,46 +50,37 @@ const route = useRoute();
 const activeKey = route.params.category;
 
 const initSearchParams = {
+  type: activeKey,
   text: "",
   pageSize: 10,
   pageNum: 1
 };
 
+const searchText = ref(route.query.text || "");
+
+/**
+ * 加载单个类型的数据
+ * @param params
+ */
 const loadData = (params: any) => {
-  // 查询参数
-  const postQuery = {
+  // 根据type来判断，需要查询那个接口。
+  const { type } = params;
+  if(!type) {
+    message.error("类型为空！");
+    return;
+  }
+  const query = {
     ...params,
-    searchText: params.text
+    searchText: params.text,
   };
-  // post请求必须传递 JSON 参数，哪怕是 {}
-  myAxios.post("post/list/page/vo", postQuery).then((res: any) => {
-    postList.value = res.records;
-    console.log(postList.value);
-    console.log(res);
-  });
-
-  // 查询参数
-  const userQuery = {
-    ...params,
-    userName: params.text
-  };
-  // post请求必须传递 JSON 参数，哪怕是 {}
-  myAxios.post("user/list/page/vo", userQuery).then((res: any) => {
-    userList.value = res.records;
-    console.log(userList.value);
-    console.log(res);
-  });
-
-  // 查询参数
-  const pictureQuery = {
-    ...params,
-    searchText: params.text
-  };
-// post请求必须传递 JSON 参数，哪怕是 {}
-  myAxios.post("picture/list/page/vo", pictureQuery).then((res: any) => {
-    pictureList.value = res.records;
-    console.log(pictureList.value);
-    console.log(res);
+  myAxios.post("searchAll/list/page/vo", query).then((res: any) => {
+    if (type === "post") {
+      postList.value = res.dataList;
+    } else if (type === "user") {
+      userList.value = res.dataList;
+    } else if (type === "picture") {
+      pictureList.value = res.dataList;
+    }
   });
 };
 
@@ -107,6 +99,7 @@ const loadAllData = (params: any) => {
 
 // 抽象一个搜索关键词
 const searchParams = ref(initSearchParams);
+
 loadAllData(searchParams);
 // 动态监听变量
 watchEffect(() => {
@@ -115,14 +108,17 @@ watchEffect(() => {
     // 将所输入内容，半持久化存储
     text: route.query.text
   } as any; //as any用于覆盖 TypeScript 的类型检查，允许将类型为 any 的值赋给 searchParam.value 对象。
-  // loadData(searchParams.value);
-  loadAllData(searchParams.value);
+  loadData(searchParams.value);
+  // loadAllData(searchParams.value);
 });
 
 const onSearch = (value: string) => {
   console.log(value);
   router.push({
-    query: searchParams.value,
+    query: {
+      ...searchParams.value,
+      text: value,
+    },
   });
   loadAllData(searchParams.value);
 };
@@ -131,7 +127,7 @@ const onTabChange = (key: string) => {
   router.push({
     //直接改变 url
     path: `/${key}`,
-    query: searchParams.value,
+    query: searchParams.value
   });
 };
 </script>
